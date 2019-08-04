88164b36bb6d364304e1ad9a924ffca65027eb3a
Tzu-Chin <53668363+Tzu-Chin@users.noreply.github.com>
2019-08-04 09:18:41 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+
+var test = document.querySelector('h1');
+
+test.onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
